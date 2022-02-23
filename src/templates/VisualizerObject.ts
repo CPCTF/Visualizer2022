a8db207cf3cbb7452eb31ed843b0ae3e32812f6f
fogrexon <fogrexon@gmail.com>
2022-02-23 18:44:05 +0900

    object template

---
+++ src/templates/VisualizerObject.ts
@@ -0,0 +1,27 @@
+import { Mesh } from 'three'
+import type { IRenderable } from './IRenderable'
+import { VisualizerGroup } from './VisualizerGroup'
+
+export abstract class VisualizerObject extends Mesh implements IRenderable {
+  private initialized = false
+  constructor() {
+    super()
+  }
+  public abstract start(): void
+
+  public update(delta: number) {
+    if (!this.visible) return
+    if (!this.initialized) {
+      this.start()
+      this.initialized = true
+    }
+    this.children.map(value => {
+      if (
+        value instanceof VisualizerGroup ||
+        value instanceof VisualizerObject
+      ) {
+        value.update(delta)
+      }
+    })
+  }
+}
