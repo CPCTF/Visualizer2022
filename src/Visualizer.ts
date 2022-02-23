8b1552b6154152ea9b94e8eb1eba49fd9c1be051
fogrexon <fogrexon@gmail.com>
2022-02-24 00:04:37 +0900

    change initializer

--- src/Visualizer.ts
+++ src/Visualizer.ts
@@ -1,32 +1,74 @@
-import { Camera, Object3D, Scene, WebGLRenderer } from 'three'
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { Time } from './system/Time'
+import { UserManager } from './system/UserManager'
+import { WebSocketReceiver } from './system/WebSocketReceiver'
+import { VisualizerGroup } from './templates/VisualizerGroup'
+import { VisualizerObject } from './templates/VisualizerObject'
+import { ThreeResourceLoader } from './system/Loader'
 
-export class Visualizer {
-  private renderer: WebGLRenderer
-  private composer: EffectComposer
-  private scene: Scene
-  private camera: Camera
-  constructor(camera: Camera) {
-    this.renderer = new WebGLRenderer({
-      canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
-      alpha: true,
-      antialias: true
+export const RunVisualizer = async () => {
+  // setup variables
+  const canvas = document.getElementById('main-canvas') as HTMLCanvasElement
+  const renderer = new WebGLRenderer({
+    canvas,
+    alpha: true,
+    antialias: true
+  })
+  const composer = new EffectComposer(renderer)
+
+  const scene = new Scene()
+
+  const camera = new PerspectiveCamera()
+
+  const loader = new ThreeResourceLoader()
+  const users = new UserManager()
+
+  const websocketReceiver = new WebSocketReceiver()
+
+  const timer = new Time()
+
+  // call methods
+  composer.addPass(new RenderPass(scene, camera))
+
+  websocketReceiver.addEventListener('start', () => {
+    console.log('start ctf')
+  })
+  websocketReceiver.addEventListener('end', () => {
+    console.log('end ctf')
+  })
+  websocketReceiver.addEventListener('recalcurate', () => {
+    console.log('recalcurate')
+  })
+  websocketReceiver.addEventListener('submit', () => {
+    console.log('submit')
+  })
+
+  // load resources
+  loader.addGLTF('gltf-path')
+  loader.addTexture('texture-path')
+  await loader.load(({ total, count }) => {
+    console.log('progress' + count / total)
+  })
+  console.log('loaded')
+
+  // animation loop
+  const tick = (timestamp: number) => {
+    requestAnimationFrame(tick)
+    timer.update(timestamp)
+
+    composer.render()
+
+    scene.children.map(value => {
+      if (
+        value instanceof VisualizerGroup ||
+        value instanceof VisualizerObject
+      ) {
+        value.update()
+      }
     })
-    this.composer = new EffectComposer(this.renderer)
-    this.scene = new Scene()
-    this.camera = camera
-
-    this.composer.addPass(new RenderPass(this.scene, this.camera))
-    this.tick()
   }
 
-  public add(...args: Object3D[]) {
-    this.scene.add(...args)
-  }
-
-  private tick() {
-    requestAnimationFrame(this.tick.bind(this))
-    this.renderer.render(this.scene, this.camera)
-  }
+  requestAnimationFrame(tick)
 }
