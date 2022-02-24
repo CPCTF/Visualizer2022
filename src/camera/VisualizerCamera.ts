4086ae8c23610c1a299ff59f69636aa3642cc75b
fogrexon <fogrexon@gmail.com>
2022-02-24 22:42:53 +0900

    camera

---
+++ src/camera/VisualizerCamera.ts
@@ -0,0 +1,18 @@
+import { Time } from '#/system/Time'
+import type { IRenderable } from '#/templates/IRenderable'
+import { PerspectiveCamera } from 'three'
+
+export class VisualizerCamera extends PerspectiveCamera implements IRenderable {
+  constructor() {
+    super(60, 1, 1, 2000)
+  }
+
+  public start(): void {
+    // no impl
+  }
+
+  public update(): void {
+    this.position.set(Math.sin(Time.time) * 3, 2, Math.cos(Time.time) * 3)
+    this.lookAt(0, 0, 0)
+  }
+}
