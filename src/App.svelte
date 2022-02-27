377bd4e61f45797a37e9a7427a218b2ab4e6cb4e
fogrexon <fogrexon@gmail.com>
2022-02-28 01:36:35 +0900

    svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let num: number = 0
+
+  setInterval(() => {
+    num += 1
+  }, 100)
+</script>
+
+<div id="canvas-wrapper">
+  <canvas id="main-canvas"></canvas>
+</div>
+
+{num}
+
+<style>
+  #canvas-wrapper {
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import autoPreprocess from 'svelte-preprocess'
+import sass from 'sass'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
+  plugins: [
+    svelte({
+      emitCss: false,
+      preprocess: autoPreprocess()
+    }),
+    tsconfigPaths()
+  ]
 })
