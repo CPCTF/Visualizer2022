d583d24eb1f749a2a280b3998df420a7a533a09e
fogrexon <fogrexon@gmail.com>
2022-03-03 21:54:28 +0900

    context

--- src/window/App.tsx
+++ src/window/App.tsx
@@ -1,3 +1,43 @@
-import React from 'react'
+import { Container, Stage as PixiStage, withFilters } from '@inlet/react-pixi'
+import { ReactNode, useContext, VFC } from 'react'
+import { WindowSettingContext, WindowSettingProvider } from './GlobalSetting'
+import { Screen } from './screen/Screen'
 
-export const App = () => <div>div</div>
+// the context bridge:
+const ContextBridge: VFC<{children: ReactNode, Context: typeof WindowSettingContext, render: (children: ReactNode) => ReactNode}> = ({ children, Context, render }) => {
+  return (
+    <Context.Consumer>
+      {value => render(<Context.Provider value={value}>{children}</Context.Provider>)}
+    </Context.Consumer>
+  )
+}
+
+export const Stage: VFC<{children: ReactNode} & Record<string, any>> = ({ children, ...props }) => {
+  return (
+    <ContextBridge Context={WindowSettingContext} render={children  => <PixiStage {...props}>{children}</PixiStage>}>
+      {children}
+    </ContextBridge>
+  )
+}
+
+const Filters = withFilters(Container, {})
+
+export const AppInner = () => {
+  const { width, height }= useContext(WindowSettingContext)
+  return (
+    <Stage width={width} height={height}>
+      <Filters>
+        <Screen />
+        {/* <Footer /> */}
+      </Filters>
+    </Stage>
+  )
+}
+
+export const App = () => {
+  return (
+    <WindowSettingProvider>
+      <AppInner />
+    </WindowSettingProvider>
+  )
+}
