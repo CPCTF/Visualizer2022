d7213805a7b69a4671901618634643be609cedc1
fogrexon <fogrexon@gmail.com>
2022-03-03 23:46:49 +0900

    frame

--- src/window/screen/Screen.tsx
+++ src/window/screen/Screen.tsx
@@ -1,7 +1,19 @@
-import { Background } from "./background/Background";
+import { useContext } from 'react'
+import { WindowSettingContext } from '../GlobalSetting'
+import { Background } from './background/Background'
+import { Frame } from './Frame'
+import { dummyWindow } from './frames'
 
-export const Screen = () => (
-  <>
-    <Background />
-  </>
-)
+export const Screen = () => {
+  const {
+    windowSettings: { windows, windowIndicies }
+  } = useContext(WindowSettingContext)
+  return (
+    <>
+      <Background />
+      {windowIndicies.map(id => (
+        <Frame windowInfo={windows[id] || dummyWindow} id={id} focus={false} />
+      ))}
+    </>
+  )
+}
--- src/window/screen/background/Background.tsx
+++ src/window/screen/background/Background.tsx
@@ -1,11 +1,11 @@
-import { footerHeight } from "#/window/globals";
-import { WindowSettingContext } from "#/window/GlobalSetting";
-import { Sprite } from "@inlet/react-pixi";
-import { useContext } from "react";
+import { footerHeight } from '#/window/globals'
+import { WindowSettingContext } from '#/window/GlobalSetting'
+import { Sprite } from '@inlet/react-pixi'
+import { useContext } from 'react'
 import bgImgSrc from './xp.jpg'
 
 export const Background = () => {
-  const {width, height} = useContext(WindowSettingContext)
+  const { width, height } = useContext(WindowSettingContext)
   console.log(width, height)
   return (
     <Sprite
