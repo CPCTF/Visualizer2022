34bfa6705762032aa691c874e770afbe64236414
fogrexon <fogrexon@gmail.com>
2022-03-10 11:00:06 +0900

    saikeisan

--- src/scene/Recalculates/index.ts
+++ src/scene/Recalculates/index.ts
@@ -1,24 +1,36 @@
+import { ThreeResourceLoader } from '#/system/Loader'
+import { Time } from '#/system/Time'
 import { WebSocketInstance } from '#/system/WebSocketReceiver'
 import { VisualizerGroup } from '#/templates/VisualizerGroup'
+import type { Material, Mesh, Object3D } from 'three'
 import { RecalculateAnimations } from './Animations'
+import recalculatingSrc from './recalculating.glb?url'
 
 export class Recalculate extends VisualizerGroup {
+  private recLabel: Object3D
   constructor() {
     super()
-    this.add(new RecalculateAnimations())
+    const animation = new RecalculateAnimations()
+    animation.scale.set(0.5, 0.5, 0.5)
+    this.add(animation)
     WebSocketInstance.addEventListener('recalculate', () => {
       ;(this.children[0] as RecalculateAnimations)?.animate()
     })
+    this.position.set(0, -10, 0)
 
-    // for test
-    const tick = () => {
-      ;(this.children[0] as RecalculateAnimations)?.animate()
-      setTimeout(tick, 15000)
-    }
-    tick()
+    // 再計算ラベル
+    this.recLabel = ThreeResourceLoader.get(recalculatingSrc) as Mesh
+    ;((this.recLabel.children[0] as Mesh).material as Material).transparent =
+      true
+    this.recLabel.position.set(0, 0.5, 0)
+    this.recLabel.scale.set(1.5, 1.5, 1.5)
+    this.add(this.recLabel)
   }
 
   // public start() {}
 
-  // public update() {}
+  public update() {
+    super.update()
+    this.recLabel.rotation.y += Time.deltaTime
+  }
 }
