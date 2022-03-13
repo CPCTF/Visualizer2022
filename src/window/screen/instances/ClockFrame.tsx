b63861152315cdf995edc987d4c550c0df1e625e
fogrexon <fogrexon@gmail.com>
2022-03-14 02:13:35 +0900

    demo app

--- src/window/screen/instances/ClockFrame.tsx
+++ src/window/screen/instances/ClockFrame.tsx
@@ -2,6 +2,12 @@ import type { WindowComponentProps } from '#/window/stores/WindowSystem'
 import { useState, VFC } from 'react'
 import { Graphics, TextStyle } from 'pixi.js'
 import { Container, Text, useTick } from '@inlet/react-pixi'
+import { Visualizer } from '#/Visualizer'
+import { globalSettings } from '#/system/GlobalSettings'
+
+const twoPadding = (time: number) => {
+  return ('00' + time).slice(-2)
+}
 
 export const ClockFrame: VFC<WindowComponentProps> = ({
   x,
@@ -10,12 +16,22 @@ export const ClockFrame: VFC<WindowComponentProps> = ({
   height
 }) => {
   const [time, setTime] = useState('')
+  const [restTime, setRestTime] = useState('')
   useTick(() => {
     const now = new Date()
     setTime(
-      `${now.getHours()}:${('00' + now.getMinutes()).slice(-2)}:${(
-        '00' + now.getSeconds()
-      ).slice(-2)}`
+      `${now.getHours()}:${twoPadding(now.getMinutes())}:${twoPadding(
+        now.getSeconds()
+      )}`
+    )
+    if (!Visualizer.getInstance().isSetupped) return
+    // minutes
+    const delta = Math.floor(
+      (globalSettings.endTime.getTime() - now.getTime()) / 1000 / 60
+    )
+    // console.log(delta.getMilliseconds())
+    setRestTime(
+      `残り: ${Math.floor(delta / 60)}時間${twoPadding(delta % 60)}分`
     )
   })
 
@@ -30,6 +46,19 @@ export const ClockFrame: VFC<WindowComponentProps> = ({
             align: 'right',
             fontFamily: 'Roboto, Helvetica, sans-serif',
             fontSize: 50,
+            fill: '#000000'
+          })
+        }
+      />
+      <Text
+        text={restTime}
+        anchor={0.5}
+        position={[width / 2, height / 2 - 50]}
+        style={
+          new TextStyle({
+            align: 'right',
+            fontFamily: 'Roboto, Helvetica, sans-serif',
+            fontSize: 30,
             fill: '#000000'
           })
         }
