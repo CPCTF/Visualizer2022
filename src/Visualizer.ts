85dbaf929d8fe86a5bbe0dacd74d5d0c2285484e
fogrexon <fogrexon@gmail.com>
2022-03-14 01:57:20 +0900

    update visualizer system

--- src/Visualizer.ts
+++ src/Visualizer.ts
@@ -12,92 +12,116 @@ import { VisualizerCamera } from './camera/VisualizerCamera'
 import { Recalculate } from './scene/Recalculates'
 import { getInitialData } from './utils/getInitialData'
 
-export interface SetupVisualizerReturn {
-  resizeHandler: (width: number, height: number) => void
-  startVisualizer: () => void
-}
-export const SetupVisualizer = (
-  canvas: HTMLCanvasElement
-): SetupVisualizerReturn => {
-  // setup variables
-  const renderer = new WebGLRenderer({
-    canvas,
-    alpha: true,
-    antialias: true
-  })
-  const composer = new EffectComposer(renderer)
-  const camera = new VisualizerCamera()
-
-  // resize event
-  const resizeHandler = (width: number, height: number) => {
-    renderer.setSize(width, height)
-    renderer.setPixelRatio(window.devicePixelRatio)
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
+export class Visualizer extends EventTarget {
+  // singleton
+  private static instance: Visualizer | null
+  public static getInstance() {
+    if (this.instance) return this.instance
+    this.instance = new Visualizer()
+    return this.instance
   }
 
-  const scene = new Scene()
-  scene.add(
-    new Effects(),
-    new UserDisplayGroup(),
-    new MainCircuit(),
-    new Recalculate()
-  )
+  private renderer: WebGLRenderer | null = null
+  private camera: VisualizerCamera | null = null
+  private tick: ((timestamp: number) => void) | null = null
 
-  // load resources
-  // ThreeResourceLoader.addGLTF('gltf-path')
-  // ThreeResourceLoader.addTexture('texture-path')
-  // await ThreeResourceLoader.load(({ total, count }) => {
-  //   console.log('progress' + count / total)
-  // })
-  // console.log('loaded')
+  // flags
+  private _isSetupped = false
+  // start called but not setupped
+  private isStartCalled = false
+  public get isSetupped() {
+    return this._isSetupped
+  }
 
-  // server connection
-  WebSocketInstance.addEventListener('start', () => {
-    console.log('start ctf')
-  })
-  WebSocketInstance.addEventListener('end', () => {
-    console.log('end ctf')
-  })
-  WebSocketInstance.addEventListener('recalculate', async () => {
-    console.log('recalculate')
-    // const { ranking, circuit } = await ServerRequest.recalculate()
-  })
-  WebSocketInstance.addEventListener('submit', () => {
-    console.log('submit')
-  })
+  public resize(width: number, height: number) {
+    if (!this.renderer || !this.camera) return
+    this.renderer.setSize(width, height)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.camera.aspect = width / height
+    this.camera.updateProjectionMatrix()
+  }
 
-  // const initialData = await ServerRequest.initial()
+  public setup(canvas: HTMLCanvasElement) {
+    const renderer = new WebGLRenderer({
+      canvas,
+      alpha: true,
+      antialias: true
+    })
+    const composer = new EffectComposer(renderer)
+    const camera = new VisualizerCamera()
 
-  // set render path
-  composer.addPass(new RenderPass(scene, camera))
+    const scene = new Scene()
+    scene.add(
+      new Effects(),
+      new UserDisplayGroup(),
+      new MainCircuit(),
+      new Recalculate()
+    )
 
-  // animation loop
-  const tick = (timestamp: number) => {
-    requestAnimationFrame(tick)
-    Time.update(timestamp)
+    // load resources
+    // ThreeResourceLoader.addGLTF('gltf-path')
+    // ThreeResourceLoader.addTexture('texture-path')
+    // await ThreeResourceLoader.load(({ total, count }) => {
+    //   console.log('progress' + count / total)
+    // })
+    // console.log('loaded')
 
-    camera.update()
-    scene.children.map(value => {
-      if (
-        value instanceof VisualizerGroup ||
-        value instanceof VisualizerObject
-      ) {
-        value.update()
-      }
+    // server connection
+    WebSocketInstance.addEventListener('start', () => {
+      console.log('start ctf')
+    })
+    WebSocketInstance.addEventListener('end', () => {
+      console.log('end ctf')
+    })
+    WebSocketInstance.addEventListener('recalculate', async () => {
+      console.log('recalculate')
+      // const { ranking, circuit } = await ServerRequest.recalculate()
+    })
+    WebSocketInstance.addEventListener('submit', () => {
+      console.log('submit')
     })
 
-    renderer.render(scene, camera)
+    // const initialData = await ServerRequest.initial()
+
+    // set render path
+    composer.addPass(new RenderPass(scene, camera))
+
+    // animation loop
+    this.tick = (timestamp: number) => {
+      if (this.tick) requestAnimationFrame(this.tick)
+      Time.update(timestamp)
+
+      camera.update()
+      scene.children.map(value => {
+        if (
+          value instanceof VisualizerGroup ||
+          value instanceof VisualizerObject
+        ) {
+          value.update()
+        }
+      })
+
+      renderer.render(scene, camera)
+    }
+
+    this.renderer = renderer
+    this.camera = camera
+
+    getInitialData().then(() => {
+      this._isSetupped = true
+      this.dispatchEvent(new CustomEvent('setupped'))
+      if (this.isStartCalled) this.start()
+    })
   }
 
-  getInitialData()
-
-  return {
-    resizeHandler,
-    startVisualizer: () => {
+  public start() {
+    if (this._isSetupped && this.tick) {
       Time.start(performance.now())
-      camera.start()
-      requestAnimationFrame(tick)
+      this.camera?.start()
+      requestAnimationFrame(this.tick)
+      this.dispatchEvent(new CustomEvent('started'))
+    } else {
+      this.isStartCalled = true
     }
   }
 }
--- src/utils/getInitialData.ts
+++ src/utils/getInitialData.ts
@@ -1,18 +1,24 @@
 import type { InitialRaw, UserRankingRaw } from '#/system/ResponseType'
 import { ServerRequest } from '#/system/ServerRequest'
-import { globalSettings } from '#/system/SettingStore'
+import { globalSettings } from '#/system/GlobalSettings'
 import { UserManager } from '#/system/UserManager'
 
 // setup with initial data
 export const getInitialData = async () => {
-  const data = (await ServerRequest.initial()) as InitialRaw
+  // const data = (await ServerRequest.initial()) as InitialRaw
 
-  data.users.forEach(value => {
-    UserManager.addUser(value)
-  })
+  // data.users.forEach(value => {
+  //   UserManager.addUser(value)
+  // })
 
   // TODO: Recalculate
 
-  globalSettings.startTime = new Date(data.startTime)
-  globalSettings.endTime = new Date(data.endTime)
+  // globalSettings.startTime = new Date(data.startTime)
+  // globalSettings.endTime = new Date(data.endTime)
+  const startDate = new Date()
+  startDate.setHours(startDate.getHours() - 1)
+  const endDate = new Date()
+  startDate.setHours(startDate.getHours() + 1)
+  globalSettings.startTime = startDate
+  globalSettings.endTime = endDate
 }
