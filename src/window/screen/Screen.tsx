8a36db218c2308fbb10d1acf0dc0756cdb53fa1a
fogrexon <fogrexon@gmail.com>
2022-03-14 17:43:25 +0900

    add ?????????????????????

--- src/window/screen/Screen.tsx
+++ src/window/screen/Screen.tsx
@@ -1,19 +1,42 @@
+import { EventManagerInstance } from '#/system/EventManager'
+import { globalSettings } from '#/system/GlobalSettings'
 import { useTick } from '@inlet/react-pixi'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { WindowSettingContext } from '../GlobalSetting'
 import { clearCursorIcon, getCursorIcon } from '../stores/cursorIcon'
 import { Background } from './background/Background'
 import { Frame } from './Frame'
 import { dummyWindow } from './frames'
+import { YouAreAnIdiot } from './virus/YouAreAnIdiot'
 
 export const Screen = () => {
   const {
+    width,
+    height,
     windowSettings: { windows, windowIndices: windowIndices }
   } = useContext(WindowSettingContext)
   useTick(() => {
     document.body.style.cursor = getCursorIcon() || 'default'
     clearCursorIcon()
   })
+  const [isEnded, setIsEnded] = useState(false)
+  useEffect(() => {
+    const initialized = () => {
+      const now = new Date().getTime()
+      const end = globalSettings.endTime.getTime()
+      if (end < now) setIsEnded(true)
+    }
+    const ctfend = () => {
+      setIsEnded(true)
+    }
+    EventManagerInstance.addEventListener('initialized', initialized)
+    EventManagerInstance.addEventListener('ctfend', ctfend)
+
+    return () => {
+      EventManagerInstance.removeEventListener('initialized', initialized)
+      EventManagerInstance.removeEventListener('ctfend', ctfend)
+    }
+  }, [])
   return (
     <>
       <Background />
@@ -25,6 +48,7 @@ export const Screen = () => {
           key={id}
         />
       ))}
+      {isEnded ? <YouAreAnIdiot width={width} height={height} /> : null}
     </>
   )
 }
--- src/window/screen/virus/YouAreAnIdiot.tsx
+++ src/window/screen/virus/YouAreAnIdiot.tsx
@@ -1,7 +1,7 @@
 import { Sprite, useTick } from '@inlet/react-pixi'
 import { useState, VFC } from 'react'
-import blackSrc from './youareanidit_black.png'
-import whiteSrc from './youareanidit_white.png'
+import blackSrc from './youareanidiot_black.png'
+import whiteSrc from './youareanidiot_white.png'
 
 export const YouAreAnIdiotWindow: VFC<{
   width: number
@@ -9,11 +9,18 @@ export const YouAreAnIdiotWindow: VFC<{
   index: number
   trans: boolean
 }> = ({ index, width, height, trans }) => {
+  const windowWidth = width * 0.7
+  const windowHeight = (windowWidth / 1576) * 994
   return (
     <>
       <Sprite
         image={trans ? blackSrc : whiteSrc}
-        position={[index * 10, index * 10]}
+        position={[
+          (index * 1000) % (width - windowWidth),
+          (index * 1000) % Math.max(1, height - windowHeight)
+        ]}
+        width={windowWidth}
+        height={windowHeight}
       />
     </>
   )
