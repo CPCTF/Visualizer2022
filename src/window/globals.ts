6e6e87d1dae71300775971bf83951345f884b48c
fogrexon <fogrexon@gmail.com>
2022-03-28 17:01:49 +0900

    createTemplates

--- src/window/globals.ts
+++ src/window/globals.ts
@@ -3,3 +3,11 @@ export const footerHeight = 54 * scaling
 export const windowHeaderHeight = 32 * scaling
 export const iconSize = 64 * scaling
 export const tabWidth = 200 * scaling
+
+let volume = 0
+export const setVolume = (v: number) => {
+  volume = v
+}
+export const getVolume = () => {
+  return volume
+}
