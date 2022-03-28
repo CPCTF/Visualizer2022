6beb9c5450a4c40ae25b1cd8a02e9364f63491f5
fogrexon <fogrexon@gmail.com>
2022-03-28 11:35:23 +0900

    add sky

--- src/scene/Effects/index.ts
+++ src/scene/Effects/index.ts
@@ -1,10 +1,12 @@
 import { VisualizerGroup } from '#/templates/VisualizerGroup'
+import { Sky } from './Sky'
 import { Wave } from './Wave'
 
 export class Effects extends VisualizerGroup {
   constructor() {
     super()
     this.add(new Wave())
+    this.add(new Sky())
   }
   public start() {
     // no impl
