4fba9857fe6e0c6424586af6491015e0b6b5be21
fogrexon <fogrexon@gmail.com>
2022-03-31 01:52:50 +0900

    change sky radius

--- src/scene/Effects/Sky.ts
+++ src/scene/Effects/Sky.ts
@@ -32,32 +32,35 @@ uniform vec3 sunColorA;
 uniform vec3 sunColorB;
 uniform vec3 sunPos;
 uniform float time;
+uniform float radius;
 varying vec3 vUv;
 varying vec4 vWorldPos;
 varying vec4 vViewPos;
 varying vec4 vScreenPos;
 
 void main() {
-  vec4 sky = vec4(mix(colorA, colorB, 1.0 - pow(1.0 - smoothstep(0.0, 1000.0, vWorldPos.y), 16.0)), 1.0);
-  vec4 sun = vec4(mix(sunColorA, sunColorB, smoothstep(0.0, 100.0, vWorldPos.y)), 1.0);
-  vec4 sunBloom = sun * exp(-distance(vWorldPos.xyz, sunPos) * 0.005);
-  sun = sun * step(distance(vWorldPos.xyz + vec3(0.0, sin(time * 0.01) * 100.0, sin(vWorldPos.y * 0.1 + time * 0.4) * pow(max(0.0, sunPos.y - vWorldPos.y) * 0.01, 4.0)), sunPos), 300.0) + sunBloom;
+  vec4 sky = vec4(mix(colorA, colorB, 1.0 - pow(1.0 - smoothstep(0.0, radius, vWorldPos.y), 16.0)), 1.0);
+  vec4 sun = vec4(mix(sunColorA, sunColorB, smoothstep(0.0, radius * 0.1, vWorldPos.y)), 1.0);
+  vec4 sunBloom = sun * exp(-distance(vWorldPos.xyz, sunPos) / radius * 5.0);
+  sun = sun * step(distance(vWorldPos.xyz, sunPos), radius * 0.3) + sunBloom;
   gl_FragColor = sky + sun;
 }
 `
 
 export class Sky extends VisualizerObject {
   constructor() {
+    const radius = 100
     super(
-      new SphereBufferGeometry(1000),
+      new SphereBufferGeometry(radius),
       new ShaderMaterial({
         uniforms: {
+          radius: { value: radius },
           colorA: { value: new Color('#db2784') },
           colorB: { value: new Color('#1c1330') },
           sunColorA: { value: new Color('#db2784') },
           sunColorB: { value: new Color('#f2ca2b') },
           sunPos: {
-            value: new Vector3(-1000, 0, 0).applyEuler(
+            value: new Vector3(-radius, 0, 0).applyEuler(
               new Euler(0.0, 0.0, -Math.PI * 0.06)
             )
           },
