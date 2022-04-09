08ca77967e276ae14a4bc980b3ac09268aa208d2
fogrexon <fogrexon@gmail.com>
2022-04-09 10:14:02 +0900

    add frame edge

--- src/window/globals.ts
+++ src/window/globals.ts
@@ -1,9 +1,10 @@
 const scaling = 1.5
 export const footerHeight = 54 * scaling
-export const windowHeaderHeight = 32 * scaling
 export const iconSize = 64 * scaling
 export const tabWidth = 200 * scaling
 export const windowEdge = 6 * scaling
+export const windowHeaderEdge = (windowEdge / 3) * 2
+export const windowHeaderHeight = (windowEdge / 6) * 26
 
 let volume = 0
 export const setVolume = (v: number) => {
