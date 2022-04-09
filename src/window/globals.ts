8004ef85ec4095ddfe6454bede17636d644c0dec
fogrexon <fogrexon@gmail.com>
2022-04-09 11:31:19 +0900

    add icon

--- src/window/globals.ts
+++ src/window/globals.ts
@@ -1,10 +1,10 @@
 const scaling = 1.5
-export const footerHeight = 54 * scaling
 export const iconSize = 64 * scaling
 export const tabWidth = 200 * scaling
 export const windowEdge = 6 * scaling
 export const windowHeaderEdge = (windowEdge / 3) * 2
 export const windowHeaderHeight = (windowEdge / 6) * 26
+export const footerHeight = windowHeaderHeight * 1.2
 
 let volume = 0
 export const setVolume = (v: number) => {
