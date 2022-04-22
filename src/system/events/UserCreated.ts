494d9d8f3b59122da0d011bf755dd5a791e75ff7
fogrexon <fogrexon@gmail.com>
2022-04-23 01:11:51 +0900

    fix dummy and other event

---
+++ src/system/events/UserCreated.ts
@@ -0,0 +1,11 @@
+import { UserManager } from '#/system/UserManager'
+
+export interface UserCreatedData {
+  userId: string
+  name: string
+  iconUrl: string
+}
+
+export const userCreated = ({ userId, name, iconUrl }: UserCreatedData) => {
+  UserManager.addUser({ id: userId, name, iconUrl })
+}
