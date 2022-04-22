288096270adf6a0269a2fd55ac8275374f2b4ceb
renard <forenard2002@gmail.com>
2022-04-23 03:24:43 +0900

    ダミーデータを設定

--- server/ServerMain.ts
+++ server/ServerMain.ts
@@ -3,7 +3,7 @@
 //   // @ts-ignore
 //   import('node-fetch').then(({ default: fetch }) => fetch(...args))
 import * as fs from 'fs'
-import { ServerTester } from './ServerTester'
+import { CircuitBuilder } from './CircuitBuilder'
 
 const genres = [
   'Newbie',
@@ -33,7 +33,7 @@ const serverMain = async () => {
   // const json = (await res.json()) as ServerResponse
 
   // implementation
-  const result = ServerTester.getJson()
+  const result = CircuitBuilder.build(CircuitBuilder.dummyServerResponse)
   // TODO: !!generated in local!!
   const basepath = process.env.EXPORT_DIR ?? './'
   const filepath = `${basepath}/circuit.json`
