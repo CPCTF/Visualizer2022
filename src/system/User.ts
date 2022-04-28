09da93482a3090d5058c25188c173df0c15db0ec
fogrexon <fogrexon@gmail.com>
2022-04-29 01:06:29 +0900

    connect server

--- src/system/User.ts
+++ src/system/User.ts
@@ -41,10 +41,10 @@ export class User extends EventTarget {
     this._rank = rank
     this._displayName = displayName
     if (!this._iconPath || this._iconPath !== iconPath) {
-      console.log(iconPath)
-      this._iconPath = iconPath
+      const pathname = new URL(iconPath).pathname
+      this._iconPath = pathname
       this._icon?.dispose()
-      this._icon = Loaders.texture.load(iconPath)
+      this._icon = Loaders.texture.load(pathname)
     }
   }
 }
