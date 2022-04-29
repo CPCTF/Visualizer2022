e8864ef16ad1dd490ae41d90355913001635ebca
fogrexon <fogrexon@gmail.com>
2022-04-29 10:43:54 +0900

    wip

--- src/window/screen/instances/RankingFrame.tsx
+++ src/window/screen/instances/RankingFrame.tsx
@@ -1,9 +1,12 @@
 import type { WindowComponentProps } from '#/window/stores/WindowSystem'
-import { useMemo, useState, VFC } from 'react'
+import { useEffect, useMemo, useState, VFC } from 'react'
 import { Graphics, TextStyle } from 'pixi.js'
 import { Container, Text, useTick } from '@inlet/react-pixi'
 import { Visualizer } from '#/Visualizer'
 import { globalSettings } from '#/system/GlobalSettings'
+import { EventEmitter } from '#/system/EventEmitter'
+import { UserManager } from '#/system/UserManager'
+import type { User } from '#/system/User'
 
 const twoPadding = (time: number) => {
   return ('00' + time).slice(-2)
@@ -15,35 +18,45 @@ export const RankingFrame: VFC<WindowComponentProps> = ({
   width,
   height
 }) => {
-  const [time, setTime] = useState('')
-  const [restTime, setRestTime] = useState('')
-  useTick(() => {
-    const now = new Date()
-    setTime(
-      `${now.getHours()}:${twoPadding(now.getMinutes())}:${twoPadding(
-        now.getSeconds()
-      )}`
-    )
-    if (!Visualizer.getInstance().isInitialized) return
-    // minutes
-    const delta = Math.ceil(
-      (globalSettings.endTime.getTime() - now.getTime()) / 1000 / 60
-    )
-    // console.log(delta.getMilliseconds())
-    setRestTime(
-      `残り: ${Math.floor(delta / 60)}時間${twoPadding(delta % 60)}分`
-    )
+  const [ranking, setRanking] = useState<User[]>(() => {
+    return UserManager.ranking.map(value => {
+      return UserManager.getUser(value) as User
+    })
   })
+  console.log(ranking)
+  const [updating, setUpdating] = useState(true)
+  useEffect(() => {
+    const recalculateStartHandler = () => {
+      setUpdating(true)
+    }
+    const recalculateEndHandler = () => {
+      setUpdating(false)
+      setRanking(
+        UserManager.ranking.map(value => {
+          return UserManager.getUser(value) as User
+        })
+      )
+      console.log(ranking)
+    }
 
-  return (
-    <Container
-      mask={useMemo(
-        () => new Graphics().drawRect(x, y, width, height),
-        [x, y, width, height]
-      )}
-    >
+    EventEmitter.on('recalculatestart', recalculateStartHandler)
+    EventEmitter.on('recalculateend', recalculateEndHandler)
+
+    return () => {
+      EventEmitter.off('recalculatestart', recalculateStartHandler)
+      EventEmitter.off('recalculateend', recalculateEndHandler)
+    }
+  }, [])
+
+  const mask = useMemo(
+    () => new Graphics().drawRect(x, y, width, height),
+    [x, y, width, height]
+  )
+
+  if (updating) {
+    ;<Container mask={mask}>
       <Text
-        text={time}
+        text={'更新中'}
         anchor={0.5}
         position={[width / 2, height / 2]}
         style={
@@ -55,19 +68,29 @@ export const RankingFrame: VFC<WindowComponentProps> = ({
           })
         }
       />
-      <Text
-        text={restTime}
-        anchor={0.5}
-        position={[width / 2, height / 2 - 50]}
-        style={
-          new TextStyle({
-            align: 'right',
-            fontFamily: 'GNUUnifont, Roboto, Helvetica, sans-serif',
-            fontSize: 30,
-            fill: '#000000'
-          })
-        }
-      />
+    </Container>
+  }
+
+  return (
+    <Container mask={mask}>
+      {ranking.map((user, index) => {
+        return (
+          <Text
+            key={user.id}
+            text={`${index + 1} : ${user.displayName}`}
+            anchor={0.5}
+            position={[width / 2, 30 + index * 30]}
+            style={
+              new TextStyle({
+                align: 'right',
+                fontFamily: 'GNUUnifont, Roboto, Helvetica, sans-serif',
+                fontSize: 30,
+                fill: '#000000'
+              })
+            }
+          />
+        )
+      })}
     </Container>
   )
 }
