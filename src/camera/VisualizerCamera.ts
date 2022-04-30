3717e90e81ce558687a14f1000647a351b2f22df
renard <forenard2002@gmail.com>
2022-04-30 16:47:21 +0900

    回路を映すカメラの実装

--- src/camera/VisualizerCamera.ts
+++ src/camera/VisualizerCamera.ts
@@ -2,35 +2,121 @@ import { Time } from '#/system/Time'
 import { EventEmitter } from '#/system/EventEmitter'
 import type { IRenderable } from '#/templates/IRenderable'
 import gsap from 'gsap'
-import { PerspectiveCamera } from 'three'
+import {
+  Euler,
+  Object3D,
+  PerspectiveCamera,
+  Quaternion,
+  Vector2,
+  Vector3
+} from 'three'
+import { CircuitManager } from '#/circuit/CliantScript/CircuitManager'
+import type { CircuitBasisInfo } from '#/circuit/BothScript/CircuitInfo'
 
 export class VisualizerCamera extends PerspectiveCamera implements IRenderable {
+  private state: VisualizerCameraState = 'Basis'
+  private timeline = gsap.timeline()
+  private readonly minPosY = 6
   constructor() {
     super(60, 1, 1, 2000)
+    /*
     EventEmitter.on('recalculatestart', () => {
       gsap.to(this.position, 2, { y: 20 })
     })
     EventEmitter.on('recalculateend', () => {
       gsap.to(this.position, 2, { y: 10 })
     })
+    */
   }
 
   public start(): void {
-    this.position.set(0, 20, 0)
-    this.position.set(
-      Math.sin(Time.time * 0.1) * 10,
-      10,
-      Math.cos(Time.time * 0.1) * 10
-    )
+    this.position.set(10, 6, 0)
     this.lookAt(0, 0, 0)
+    this.lookBasis()
   }
 
   public update(): void {
-    this.position.set(
-      Math.sin(Time.time * 0.1) * 10,
-      this.position.y,
-      Math.cos(Time.time * 0.1) * 10
-    )
-    this.lookAt(0, 0, 0)
+    this.stateMachine()
+  }
+
+  private stateMethods(): void {
+    switch (this.state) {
+      case 'Basis':
+        return this.lookBasis()
+      case 'Display':
+        return this.lookDisplay()
+    }
+  }
+
+  private stateMachine() {
+    if (!this.timeline.isActive()) {
+      console.log('stateMachine')
+      this.timeline.clear()
+      this.stateMethods()
+      this.timeline.play(0)
+    }
+  }
+
+  private readonly basisMoveTime = 12
+  private readonly basisRotTime = 2
+  private basisSideDir = 0
+  private lookBasis(): void {
+    this.basisSideDir =
+      (this.basisSideDir + 1 + Math.floor(Math.random() * 3)) % 4
+    const [sizeX, sizeZ] = CircuitManager.getBasisSize()
+    const toPosX =
+      this.basisSideDir % 2 == 0
+        ? sizeX * Math.random() - sizeX / 2
+        : this.basisSideDir == 1
+        ? sizeX / 2
+        : -sizeX / 2
+    const toPosZ =
+      this.basisSideDir % 2 == 1
+        ? sizeZ * Math.random() - sizeZ / 2
+        : this.basisSideDir == 0
+        ? sizeZ / 2
+        : -sizeZ / 2
+
+    const toPos = new Vector2(toPosX, toPosZ)
+    const rotate = this.getLookRotation(new Vector3(toPos.x, -10, toPos.x))
+    //rot
+    const tl1 = gsap.timeline()
+    /*
+    tl1.to(this.rotation, this.basisMoveTime, {
+      x: rotate.x,
+      y: rotate.y,
+      z: rotate.z
+    })*/
+    tl1
+      .call(() => {
+        this.lookAt(0, 0, 0)
+        console.log('lookAt')
+      })
+      .delay(1 / 30)
+      .repeat(1e9)
+    //move
+    const tl2 = gsap.timeline()
+    tl2.to(this.position, this.basisMoveTime, {
+      x: toPos.x,
+      z: toPos.y
+    })
+    this.timeline.add(tl1, 0)
+    this.timeline.add(tl2, 0)
+  }
+
+  private lookDisplay(): void {
+    //TODO
+  }
+
+  private getLookRotation(look: Vector3): Euler {
+    const befrot = this.rotation.clone()
+    this.lookAt(look)
+    const rotate = this.rotation.clone()
+    this.rotation.set(befrot.x, befrot.y, befrot.z)
+    return rotate
   }
 }
+
+export const VisualizerCameraStateList = ['Basis', 'Display'] as const
+// TODO: ジャンル追加
+export type VisualizerCameraState = typeof VisualizerCameraStateList[number]
--- src/scene/UserDisplayGroup/index.ts
+++ src/scene/UserDisplayGroup/index.ts
@@ -26,42 +26,6 @@ export class UserDisplayGroup extends VisualizerGroup {
   constructor() {
     super()
     this.users = []
-
-    /*
-    let displayNum = 20
-    let displays = new Array(displayNum).fill(null).map((_, index) => {
-      const user = new UserDisplay()
-      const rot = (index / displayNum) * Math.PI * 2
-      user.position.set(Math.sin(rot) * 4, 0, Math.cos(rot) * 4)
-      user.scale.set(0.4, 0.4, 0.4)
-      user.lookAt(0, -2, 0)
-      this.add(user)
-      return user
-    })
-    this.users = this.users.concat(displays)
-    displayNum = 60
-    displays = new Array(displayNum).fill(null).map((_, index) => {
-      const user = new UserDisplay()
-      const rot = (index / displayNum) * Math.PI * 2
-      user.position.set(Math.sin(rot) * 4.5, -1, Math.cos(rot) * 4.5)
-      user.scale.set(0.15, 0.15, 0.15)
-      user.lookAt(0, -2, 0)
-      this.add(user)
-      return user
-    })
-    this.users = this.users.concat(displays)
-    displayNum = 120
-    displays = new Array(displayNum).fill(null).map((_, index) => {
-      const user = new UserDisplay()
-      const rot = (index / displayNum) * Math.PI * 2
-      user.position.set(Math.sin(rot) * 5, -1.5, Math.cos(rot) * 5)
-      user.scale.set(0.1, 0.1, 0.1)
-      user.lookAt(0, -2, 0)
-      this.add(user)
-      return user
-    })
-    this.users = this.users.concat(displays)
-    */
   }
 
   public start() {
@@ -82,8 +46,8 @@ export class UserDisplayGroup extends VisualizerGroup {
       this.users[index].updateUser(id)
     })
 
-    EventEmitter.on('submit', ({ userid }) => {
-      const user = this.users.filter(user => user.userid === userid)
+    EventEmitter.on('submit', ({ userId }) => {
+      const user = this.users.filter(user => user.userid === userId)
       if (!user.length) return
       user[0].animation()
     })
